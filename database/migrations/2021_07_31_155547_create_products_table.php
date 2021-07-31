e7ae8acbee33cfd08454f59ea3cdbbe53bccf946
foysal11203004 <foysal11203004@gmail.com>
2021-07-31 17:20:04 +0600

    create categories, products,orders migrations and model

---
+++ database/migrations/2021_07_31_155547_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('name',100);
+            $table->double('price',10,2)->default(0.00);
+            $table->integer('stock')->default(0);
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->string('status',10)->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
